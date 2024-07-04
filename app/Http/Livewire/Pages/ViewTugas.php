997b73e0bf9ee820017fdd7def898498b14fd0fb
naufalamr17 <naufal.amr17@outlook.co.id>
2024-07-04 15:24:53 +0700

    Add print functional

--- app/Http/Livewire/Pages/ViewTugas.php
+++ app/Http/Livewire/Pages/ViewTugas.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\Pages;
 use App\Models\LeaveRequest;
 use Livewire\Component;
 use Livewire\WithPagination;
+use PhpOffice\PhpWord\TemplateProcessor;
 
 class ViewTugas extends Component
 {
@@ -34,5 +35,40 @@ class ViewTugas extends Component
     {
         LeaveRequest::findOrFail($id)->delete();
         session()->flash('success', 'Leave request deleted successfully.');
+    }
+
+    public function print($id)
+    {
+        // Retrieve leave request data based on $id
+        $leaveRequest = LeaveRequest::find($id);
+
+        // Check if leave request exists
+        if (!$leaveRequest) {
+            return redirect()->back()->with('error', 'Leave request not found.');
+        }
+
+        // Load your template Word document
+        $templatePath = public_path('\SuratTugasPerjalananDinas.docx');
+        $templateProcessor = new TemplateProcessor($templatePath);
+
+        // Replace placeholders in the template with actual data
+        $templateProcessor->setValue('Name', $leaveRequest->name);
+        $templateProcessor->setValue('NIK', $leaveRequest->nik);
+        $templateProcessor->setValue('Position', $leaveRequest->position);
+        $templateProcessor->setValue('Start', $leaveRequest->start_date);
+        $templateProcessor->setValue('End', $leaveRequest->end_date);
+        $templateProcessor->setValue('Destination', $leaveRequest->destination_place);
+        $templateProcessor->setValue('Purpose', $leaveRequest->activity_purpose);
+        // Add more replacements as needed
+
+        // Generate a filename for the output Word document
+        $outputFileName = 'leave_request_' . $leaveRequest->id . '.docx';
+
+        // Save the generated document
+        $outputFilePath = public_path('\leave_requests' . $outputFileName);
+        $templateProcessor->saveAs($outputFilePath);
+
+        // Example: Return a response with the generated Word document for download
+        return response()->download($outputFilePath, $outputFileName)->deleteFileAfterSend(true);
     }
 }
